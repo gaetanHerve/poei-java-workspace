21102211388688c40052975d3939c4e6c8eb2826
gaetanHerve <gaetan.herve@lilo.org>
2023-04-17 09:28:30 +0200

    add projetJspDaoPersonne

---
+++ projetJspDaoPersonne/src/main/java/srv/Servlet1.java
@@ -0,0 +1,54 @@
+package srv;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.DaoPersonne;
+import model.Personne;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DaoPersonne personneDao = new DaoPersonne();
+		try {
+			ArrayList<Personne> personnes = personneDao.select();
+			request.setAttribute("personnes", personnes);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+		request.getRequestDispatcher("page1.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
